400b18299ae048e41e4f173efd291950f82564e8
Michiel <michiel@sowmedia.nl>
2017-06-28 17:21:27 +0200

    Parse args properly

--- classes/waterfall.php
+++ classes/waterfall.php
@@ -158,7 +158,7 @@ class Waterfall {
         
         // If we already have configurations, we merge the arrays
         if( isset($this->configurations[$type]) && is_array($this->configurations[$type]) )
-            $configurations = wp_parse_args( $configurations, $this->configurations[$type] );
+            $configurations = wp_parse_args( $this->configurations[$type], $configurations );
         
         // Set our configurations
         $this->configurations[$type] = apply_filters('waterfall_' . $type, $configurations);
