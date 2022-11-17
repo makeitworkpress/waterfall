0a85260921b37dae5c4772550479bd23b1909091
leichim <michielsite@hotmail.com>
2022-11-17 12:32:26 +0100

    Updated wp-components and wp-custom-fields components, improved autoloading according to PSR-4

--- functions.php
+++ functions.php
@@ -4,14 +4,14 @@
  */
 spl_autoload_register( function($class_name) {
     
-    $called_class       = str_replace( '\\', '/', str_replace( '_', '-', strtolower($class_name) ) );
+    $called_class       = str_replace( '\\', '/', str_replace( '_', '-', $class_name ) );
     $theme_dir          = get_template_directory() . '/';
     
     // Require main parent classes
     $class_names        = explode('/', $called_class);
     $final_class        = array_pop($class_names);
     $class_rel_path     = $class_names ? implode('/', $class_names) . '/class-' . $final_class : 'class-' . $final_class;
-    $theme_class_file   = $theme_dir . 'classes/' . $class_rel_path . '.php';
+    $theme_class_file   = $theme_dir . 'classes/' . strtolower( $class_rel_path ) . '.php';
 
     if( file_exists( $theme_class_file ) ) {
         require_once( $theme_class_file );
@@ -19,7 +19,7 @@ spl_autoload_register( function($class_name) {
     } 
 
     // Child themes with WordPress class naming
-    $child_class_file   = get_stylesheet_directory() . '/classes/' . $class_rel_path . '.php';
+    $child_class_file   = get_stylesheet_directory() . '/classes/' . strtolower($class_rel_path) . '.php';
     
     if( file_exists($child_class_file) ) {
         require_once( $child_class_file );
@@ -27,15 +27,22 @@ spl_autoload_register( function($class_name) {
     } 
     
     // Child themes not using the WordPress class naming ( using a simple class- replace; namespaces in child themes should not have Class_ )
-    $child_class_file   = get_stylesheet_directory() . '/classes/' . str_replace('class-', '', $class_rel_path) . '.php';
+    $child_class_file   = get_stylesheet_directory() . '/classes/' . strtolower( str_replace('class-', '', $class_rel_path) ) . '.php';
     
     if( file_exists($child_class_file) ) {
         require_once( $child_class_file );
         return;
-    }     
+    }  
     
+        
     // Require Vendor (composer) classes
+    if( ! isset($class_names[0]) || $class_names[0] !== 'MakeitWorkPress' || ! isset($class_names[1]) ) {
+        return;
+    }
+
     array_splice($class_names, 2, 0, 'src');
+    $class_names[0] = strtolower($class_names[0]);
+    $class_names[1] = strtolower($class_names[1]);
     $vendor_class_file  = $theme_dir . 'vendor/' . implode('/', $class_names) . '/' . $final_class . '.php';
 
     if( file_exists($vendor_class_file) ) {
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,104 +1,111 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '18d78123836eb8e22fc5d6f9ee001f510eea3c3f',
+        'name' => 'makeitworkpress/waterfall',
+        'dev' => true,
     ),
-    'reference' => '83b6dec84f71219a7e11442b85eff586d0665410',
-    'name' => 'makeitworkpress/waterfall',
-  ),
-  'versions' => 
-  array (
-    'makeitworkpress/waterfall' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '83b6dec84f71219a7e11442b85eff586d0665410',
+    'versions' => array(
+        'makeitworkpress/waterfall' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '18d78123836eb8e22fc5d6f9ee001f510eea3c3f',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-components' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-components',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '8812419811a59faf87f62350a67dae53fc75922e',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-config' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-config',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '336aa79a7db39ee4b000cdd0f585540431408b21',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-custom-fields' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-custom-fields',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '420d7856b0d851498b52a05f0090dd30dccd304f',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-enqueue' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-enqueue',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '950df7dee5dde04b15fbb8a0fb4ecc6cc193a801',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-optimize' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-optimize',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'c998ffc6240adf1f3b8309babd7cfb345bd3e677',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-register' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-register',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '800fe6744f63587089f939e55ba1000e5d4785b2',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-router' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-router',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'b2375d77b321e09a6d35c9ae9e8e631c6526aba0',
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-updater' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-updater',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'eafa352d1e90ccd85035ab0476d2720354ca8a5c',
+            'dev_requirement' => false,
+        ),
     ),
-    'makeitworkpress/wp-components' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '0ca1e5bd6f4f4b5691c2671c01f1bb87fc579e95',
-    ),
-    'makeitworkpress/wp-config' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '336aa79a7db39ee4b000cdd0f585540431408b21',
-    ),
-    'makeitworkpress/wp-custom-fields' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => 'b69e204c9f423fb7e1d0b498615ea75e7ca6fbab',
-    ),
-    'makeitworkpress/wp-enqueue' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '950df7dee5dde04b15fbb8a0fb4ecc6cc193a801',
-    ),
-    'makeitworkpress/wp-optimize' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => 'c998ffc6240adf1f3b8309babd7cfb345bd3e677',
-    ),
-    'makeitworkpress/wp-register' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '800fe6744f63587089f939e55ba1000e5d4785b2',
-    ),
-    'makeitworkpress/wp-router' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => 'b2375d77b321e09a6d35c9ae9e8e631c6526aba0',
-    ),
-    'makeitworkpress/wp-updater' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => 'eafa352d1e90ccd85035ab0476d2720354ca8a5c',
-    ),
-  ),
 );
